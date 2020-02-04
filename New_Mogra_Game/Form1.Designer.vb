f437962b7ea1722d9490cc806353b16514e1f578
RitsukiShuto <rtk410s@gmail.com>
2020-02-04 11:53:51 +0900

    プロジェクト ファイルを追加します。

---
+++ New_Mogra_Game/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(273, 336)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(173, 107)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "スタート(Easy)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(867, 569)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(68, 21)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "終了"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(2, 491)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(162, 108)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS UI Gothic", 15.0!)
+        Me.Label3.Location = New System.Drawing.Point(801, 9)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "残り"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("MS UI Gothic", 15.0!)
+        Me.Label4.Location = New System.Drawing.Point(906, 10)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "秒"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("MS UI Gothic", 15.0!)
+        Me.Label5.Location = New System.Drawing.Point(906, 53)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 20)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "点"
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(477, 336)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(173, 107)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "スタート(Hard)"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 700
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 20.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(864, 10)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 27)
+        Me.Label1.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 20.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(864, 47)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 27)
+        Me.Label2.TabIndex = 10
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(2, 491)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(162, 108)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1200
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(0, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(99, 12)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "1931074 首藤立樹"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(946, 601)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label6 As Label
+End Class
---
+++ New_Mogra_Game/Form1.vb
@@ -0,0 +1,112 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+    End Sub
+
+    ' 難易度選択
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' スタートボタン(Easy)
+        ' モグラを消す
+        PictureBox1.Visible = False
+        PictureBox2.Visible = False
+        ' スタートボタンと終了ボタンを消す
+        Button1.Visible = False
+        Button2.Visible = False
+        Button3.Visible = False
+        ' 残り時間と得点を初期化
+        Label1.Text = 30
+        Label2.Text = 0
+        ' タイマーを起動
+        Timer1.Enabled = True
+        Timer2.Enabled = True
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' スタートボタン(Hard)
+        ' モグラとネコを消す
+        PictureBox1.Visible = False
+        PictureBox2.Visible = False
+        ' スタートボタンと終了ボタンを消す
+        Button1.Visible = False
+        Button2.Visible = False
+        Button3.Visible = False
+        ' 残り時間と得点を初期化
+        Label1.Text = 30
+        Label2.Text = 0
+        ' タイマーを起動
+        Timer3.Enabled = True
+        Timer2.Enabled = True
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        ' モグラが叩かれたとき
+        ' サウンド再生とポイント追加
+        ' My.Computer.Audio.Play("get.wav", AudioPlayMode.WaitToComplete)
+        Label2.Text += 1
+        If Label1.Text = 10 And Label2.Text > 13 Then
+            Label1.Text += 5
+        End If
+        PictureBox1.Visible = False
+    End Sub
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs)
+        ' ネコが叩かれたとき
+        ' サウンド再生とポイントを減らす
+        ' My.Computer.Audio.Play("lost.wav", AudioPlayMode.WaitToComplete)
+        If Label2.Text > 0 Then
+            Label2.Text -= 1
+        End If
+        PictureBox1.Visible = False
+    End Sub
+    ' イージーモード
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim a, b, x, y As Integer
+        PictureBox1.Visible = False
+        PictureBox2.Visible = False
+        ' 画像の座標を生成
+        a = Int(Rnd() * 900) + 10
+        b = Int(Rnd() * 600) + 10
+        x = Int(Rnd() * 900) + 10
+        y = Int(Rnd() * 600) + 10
+        ' 画像を移動
+        PictureBox1.Location = New Point(x, y)
+        PictureBox2.Location = New Point(a, b)
+        ' 画像を表示
+        PictureBox1.Visible = True
+        PictureBox2.Visible = True
+    End Sub
+    ' ハードモード
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        Dim a, b, x, y As Integer
+        PictureBox1.Visible = False
+        PictureBox2.Visible = False
+        ' 画像の座標を生成
+        a = Int(Rnd() * 900) + 10
+        b = Int(Rnd() * 600) + 10
+        x = Int(Rnd() * 900) + 10
+        y = Int(Rnd() * 600) + 10
+        ' 画像を移動
+        PictureBox1.Location = New Point(x, y)
+        PictureBox2.Location = New Point(a, b)
+        ' 画像を表示
+        PictureBox1.Visible = True
+        PictureBox2.Visible = True
+    End Sub
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Label1.Text = Label1.Text - 1
+        If Label1.Text = 0 Then
+            Timer1.Enabled = False
+            Timer2.Enabled = False
+            Timer3.Enabled = False
+            Button1.Visible = True
+            Button2.Visible = True
+            Button3.Visible = True
+            PictureBox1.Visible = False
+            PictureBox2.Visible = False
+        End If
+        If Label1.Text < 4 Then
+            Label1.ForeColor = Color.Red    ' 3秒以下は赤文字
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
